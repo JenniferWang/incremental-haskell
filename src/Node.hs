4a76f6800831a57ced807aaef0e41fd686a68db4
Kaixi Ruan <kaixi@DN525ekc.SUNet>
2016-02-16 14:47:54 -0800

    Node

--- src/Node.hs
+++ src/Node.hs
@@ -1,15 +1,66 @@
-module Node (
-    Node(..)
-  , Packed_node
-  , pack
-  ) where
+module Node where
 
-data Node a = Node {
-    getNode :: a
-  }
+import Data.IORef
+import Kind
 
-data Packed_node
+data NodeId = NodeId { i :: IORef Int }
 
-pack :: Node a -> Packed_node
-pack = undefined
+newId :: IO NodeId
+newId = do
+     fs <- newIORef 0
+     return (NodeId {i = fs})
 
+nextId :: NodeId -> IO Int
+nextId nodeId = do
+     id <- readIORef (i nodeId)
+     writeIORef (i nodeId) (id + 1)
+     return id
+     
+data Node = Node{
+     nid :: Int,
+     children :: [IORef Node]
+ }
+   
+
+-- TO DO TYPE NODE = IORef Node ...
+
+newNode :: NodeId -> IO Node
+newNode nodeId = do 
+       i <- nextId nodeId
+       return (Node{nid = i, children = []})
+
+showId :: Node -> Int
+module Node where
+
+import Data.IORef
+import Kind
+
+data NodeId = NodeId { i :: IORef Int }
+
+newId :: IO NodeId
+newId = do
+     fs <- newIORef 0
+     return (NodeId {i = fs})
+
+nextId :: NodeId -> IO Int
+nextId nodeId = do
+     id <- readIORef (i nodeId)
+     writeIORef (i nodeId) (id + 1)
+     return id
+     
+data Node = Node{
+     nid :: Int,
+     children :: [IORef Node]
+ }
+   
+
+-- TO DO TYPE NODE = IORef Node ...
+
+newNode :: NodeId -> IO Node
+newNode nodeId = do 
+       i <- nextId nodeId
+       return (Node{nid = i, children = []})
+
+showId :: Node -> Int
+showId n = nid n
+showId n = nid n
