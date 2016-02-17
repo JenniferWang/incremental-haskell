54a2e6fb78507d5fda4df14815a2e3663a775ca6
Kaixi Ruan <kaixi@DN525ekc.SUNet>
2016-02-16 16:44:10 -0800

    added functions to Node.hs

--- src/Node.hs
+++ src/Node.hs
@@ -1,7 +1,7 @@
 module Node where
 
 import Data.IORef
-import Kind
+--import Kind
 
 data NodeId = NodeId { i :: IORef Int }
 
@@ -15,52 +15,51 @@ nextId nodeId = do
      id <- readIORef (i nodeId)
      writeIORef (i nodeId) (id + 1)
      return id
-     
-data Node = Node{
-     nid :: Int,
-     children :: [IORef Node]
+
+data Kind a =
+    Const a
+      | Invalid
+ deriving Eq
+
+data Node a = Node{
+     nid      :: Int,
+     kind     :: Kind a,
+    -- value :: a,
+     children :: [IORef (Node a)],
+     parents  :: [IORef (Node a)]
  }
+  deriving Eq
    
 
--- TO DO TYPE NODE = IORef Node ...
-
-newNode :: NodeId -> IO Node
+newNode :: NodeId -> IO (Node a)
 newNode nodeId = do 
        i <- nextId nodeId
-       return (Node{nid = i, children = []})
+       return (Node{nid = i, kind = Invalid, children = [],parents=[]})
 
-showId :: Node -> Int
-module Node where
 
-import Data.IORef
-import Kind
+-- Checks whether n is a valid node
+-- it is if and only if its kind is valid
+isValid :: Node a -> Bool
+isValid n = case kind n of 
+         Invalid -> False
+         _       -> True
 
-data NodeId = NodeId { i :: IORef Int }
+-- Checks whether m is a child of n
+isChild :: Eq a => Node a -> Node a -> IO Bool
+isChild m n = isElem m (children n) 
 
-newId :: IO NodeId
-newId = do
-     fs <- newIORef 0
-     return (NodeId {i = fs})
 
-nextId :: NodeId -> IO Int
-nextId nodeId = do
-     id <- readIORef (i nodeId)
-     writeIORef (i nodeId) (id + 1)
-     return id
-     
-data Node = Node{
-     nid :: Int,
-     children :: [IORef Node]
- }
-   
+-- Checks whether m is a parent of n
+isParent :: Eq a => Node a -> Node a -> IO Bool
+isParent m n = isElem m (parents n) 
 
--- TO DO TYPE NODE = IORef Node ...
 
-newNode :: NodeId -> IO Node
-newNode nodeId = do 
-       i <- nextId nodeId
-       return (Node{nid = i, children = []})
-
-showId :: Node -> Int
-showId n = nid n
-showId n = nid n
+----------------------   helper functions -----------------
+-- test whether an element is present in a list of IORef a's
+isElem :: Eq a => a -> [IORef a] -> IO Bool
+isElem x [] = return False
+isElem x (y:ys) = do 
+               z <- readIORef y
+               case z == x of
+                True  -> return True
+                False -> isElem x ys
